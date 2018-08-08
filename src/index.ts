213d7a6065aabe21ffd45c232980e4df04a407b3
Elmar Athmer <elmar@athmer.org>
2018-08-08 22:34:09 +0200

    implement tryF util

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,62 @@
-console.log("Hello World");
+/**
+ * helper to test if an object is a Promise.
+ *
+ * Taken from https://github.com/ssnau/xkit/blob/master/util/is-promise.js,
+ * as suggested by https://stackoverflow.com/questions/27746304/how-do-i-tell-if-an-object-is-a-promise
+ * as is plausible considering https://promisesaplus.com/#point-53
+ */
+function isPromise(obj: unknown): obj is Promise<any> {
+    return !!obj && (typeof obj === "object" || typeof obj === "function") && typeof (obj as any).then === "function";
+}
+
+/**
+ * Union type to wrap the original type T and allow Errors additionally
+ */
+export type Try<T> = T | Error;
+
+/**
+ * simple function to turn a promise of type T to type T | Error
+ *
+ * i.e.: catch the error and return it as the value
+ */
+function tryify<T>(p: Promise<T>): Try<T> {
+    return (p as any).then((x: T) => x, (err: Error) => err);
+}
+
+/**
+ * tryF wraps code that throws, and returns the result OR the error thrown
+ *
+ * it imitates the concept (though it's not a monad) of scala.util.Try — but try is a reserved keyword, so it's called tryF
+ */
+export function tryF<T>(asyncBlock: () => Promise<T>): Promise<Try<T>>;
+export function tryF<T>(block: () => T): Try<T>;
+// tslint:disable-next-line:unified-signatures I want the name (in completion) to be promise, so I NOT unifying here is desired
+export function tryF<T>(promise: PromiseLike<T>): Promise<Try<T>>;
+export function tryF<T>(input: PromiseLike<T> | (() => T | Promise<T>)): Try<T> | Promise<Try<T>> {
+    // if the input is a simple promise, a simple tryify is enougf
+    if (isPromise(input)) {
+        return tryify(input);
+    }
+
+    // ok, the input is a (sync or async) function, we need to execute it
+    const block: () => T | Promise<T> = input as () => T | Promise<T>;
+
+    try {
+        const v = block();
+
+        // if block is an async function, tryify the returned promise
+        if (isPromise(v)) {
+            return tryify(v);
+        }
+
+        // if block is sync, the result is in v, so just return
+        return v;
+    } catch (err) {
+        // execution of block throwed (and it's obviously sync), so return the error
+        return err;
+    }
+}
+
+export function isError(e: unknown): e is Error {
+    return e instanceof Error;
+}
--- test/try.test.ts
+++ test/try.test.ts
@@ -1,6 +1,54 @@
 import assert from "assert";
+import {isError, tryF} from "../src";
 
 describe("tryF", () => {
 
-    it("does things", () => assert(true));
+    describe("synchronous function", () => {
+        it("returns the error thrown in a block", () => {
+            const result = tryF(() => { throw new Error("dummy"); });
+
+            assert(result instanceof Error);
+        });
+
+        it("returns the value if nothing is thrown in a block", () => {
+            const result = tryF(() => 42);
+
+            assert.strictEqual(result, 42);
+        });
+    });
+
+    describe("async functions", () => {
+        it("returns the error thrown in a block", async () => {
+            const result = await tryF(() => Promise.reject<number>(new Error("dummy")));
+
+            assert(result instanceof Error);
+        });
+
+        it("returns the value if nothing is thrown in a block", async () => {
+            const result = await tryF(() => Promise.resolve(42));
+
+            assert.strictEqual(result, 42);
+        });
+    });
+
+    describe("promises", () => {
+        it("returns the value resolved by a promise", async () => {
+            const result = await tryF(Promise.resolve(42));
+
+            assert.strictEqual(result, 42);
+        });
+
+        it("returns the reason why a promise has been rejected", async () => {
+            const result = await tryF(Promise.reject<number>(new Error("dummy")));
+
+            assert(result instanceof Error);
+        });
+
+    });
+});
+
+describe("isError()", () => {
+    it("returns true for errors", () => assert(isError(new Error("dummy"))));
+
+    it("returns false for something that's not an error", () => assert(!isError({})));
 });
